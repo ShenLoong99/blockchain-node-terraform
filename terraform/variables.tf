cdb79b5986d4a9ff217c493da212ce51c6061c20
ShenLoong99 <yithuettsk29@gmail.com>
2025-12-18 12:12:08 +0800

    fix user_data script and variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,9 +20,3 @@ variable "my_local_ip" {
   type        = string
   description = "My local public IP address (e.g., 1.2.3.4/32)"
 }
-
-# variable "ssh_public_key" {
-#   type        = string
-#   description = "Public SSH key for EC2 access"
-#   default     = file("blockchain-node-key.pub")
-# }
